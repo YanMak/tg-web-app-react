b9be9799baafb945039ac7009f4f4c6d0e31f8ef
Yan <yan@MacBook-Pro-Aydin.local>
2023-04-15 09:30:32 +0300

    Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import './App.css';
+import Header from './components/Header/Header';
 
 import {useTelegram} from "./hooks/useTelegram";
 
@@ -19,6 +20,7 @@ function App() {
   return (
     <div className="App">
       work!!!
+      <Header/>
       <button onClick={()=>onClose()}>Закрыть</button>
     </div>
   );
